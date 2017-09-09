71528c6d5e7069be6d51267cfc7c5e227ecfc47d
MonikaP <piechatzek@hotmail.com>
2017-09-09 14:44:48 -0700

    added font awesome icons for header

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -12,13 +12,16 @@ const Feed = () => {
           <div className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked" id="tuckedMenu">
               <div className="custom-menu-screen"></div>
             <ul className="pure-menu-list">
-                  <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                  <li className="pure-menu-item"><a href="#" className="pure-menu-link"><i className="fa fa-search fa-2x" aria-hidden="true"></i></a></li>
                 <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link"><i className="fa fa-heart-o fa-2x" aria-hidden="true"></i></a></li>
+            <li className="pure-menu-item"><a href="#" className="pure-menu-link"><i className="fa fa-user-o fa-2x" aria-hidden="true"></i></a></li>
               </ul>
           </div>
       </div>
+
+      <p>feed</p>
+    <i className="fa fa-camera-retro fa-lg"></i>
     </div>
 
 
