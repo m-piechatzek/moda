f3424d2ef562beeb10b3718dac4e36b0d4c057a2
MonikaP <piechatzek@hotmail.com>
2017-09-09 13:57:04 -0700

    new images folder and basic header working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Feed from './components/Feed'
 import Profile from './components/Profile'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+      <div>
+
+        {/* Header will eventually go here */}
 
         <Switch>
           <Route exact path="/" component={Feed} />
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import './Feed.css';
 
 const Feed = () => {
   return (
-    <p>Feed</p>
+    <div>
+    <div className="custom-menu-wrapper">
+          <div className="pure-menu custom-menu custom-menu-top">
+              <a href="#" className="pure-menu-heading custom-menu-brand">Brand</a>
+            <a href="#" className="custom-menu-toggle" id="toggle"><s className="bar"></s><s className="bar"></s></a>
+          </div>
+          <div className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked" id="tuckedMenu">
+              <div className="custom-menu-screen"></div>
+            <ul className="pure-menu-list">
+                  <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Blog</a></li>
+              </ul>
+          </div>
+      </div>
+    </div>
+
+
   )
 }
 
