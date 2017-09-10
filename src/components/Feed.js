2643c2296a21c4d8415e130231abe775c8523e9a
MonikaP <piechatzek@hotmail.com>
2017-09-09 20:27:04 -0700

    Added 4 blogs and grayscale transition hover on them

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,7 +6,13 @@ const Feed = () => {
     <div>
 
       <img className="blog-size" src={"/images/blog_color1.png"} />
-
+    <img className="blog-size" src={"/images/blog_color2.png"} />
+  <img className="blog-size" src={"/images/blog_color3.png"} />
+<img className="blog-size" src={"/images/blog_color4.png"} />
+<img className="blog-size" src={"/images/blog_color5.png"} />
+<img className="blog-size" src={"/images/blog_color6.png"} />
+<img className="blog-size" src={"/images/blog_color7.png"} />
+<img className="blog-size" src={"/images/blog_color8.png"} />
     </div>
 
 
