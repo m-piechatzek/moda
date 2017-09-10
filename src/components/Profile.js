8342725cec7c7714166896f4753ad27c2e29bbc7
Francesca Ramunno <franramunno@gmail.com>
2017-09-09 20:40:12 -0700

    More posts, price tags and links on hover

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,17 @@ import React from 'react'
 import './profile.css';
 
 const Profile = () => {
+  function switchPic() {
+    document.getElementById("blog2").src="/images/fake-profile-2-prices.jpg";
+    console.log("test");
+  }
+  function switchPicBack() {
+    document.getElementById("blog2").src="/images/fake-profile-2.jpg";
+    console.log("test");
+  }
+  function goToLink() {
+    window.open("https://www.anthropologie.com/shop/matiko-michelle-block-heel-sandals?category=shoes&color=072");
+  }
   return (
     <div id="mainContainer">
         <img classname="pure-img" id="headShot" src="images/fake-profile-1.jpg"></img>
@@ -35,7 +46,10 @@ const Profile = () => {
 
                 <div classname = "pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
                    <div classname = "graybox">
-                      <img classname="pure-img" id="blog" src="images/fake-profile-2.jpg"></img>
+                      <img id="blog2" src="images/fake-profile-2.jpg"
+                      onMouseOver={switchPic}
+                      onMouseOut={switchPicBack}
+                      onClick={goToLink}></img>
                       <h2>Oversized Sleeves</h2>
                       <p>
                          Here’s my look from the recent Anthropologie rooftop dinner party that
@@ -45,15 +59,34 @@ const Profile = () => {
                          the many off the shoulder tops already in my closet — cause how many
                          is too many, really?
                       </p>
-                      
+                      <a href="full-post.js">read more</a>
+
                    </div>
                 </div>
 
                 <div classname = "pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
                    <div classname="graybox">
-                      <p>Second Column</p>
+                      <img id="blog" src="images/fake-profile-3.jpg"></img>
+                      <h2>A secret smartwatch</h2>
+                      <p>I’d like you to officially meet Shola. You might have noticed him sparingly
+                      in an Instagram Story or a Valentine’s Day post. A devoted Instagram Husband,
+                      he’s dedicated many a weekend or vacation day to getting the perfect shot.
+                      When Fossil asked me to try out their new hybrid smartwatch, I immediately new
+                      I had to ask if Shola could try one out too.</p>
+                      <a href="full-post.js">read more</a>
                    </div>
                 </div>
+
+                  <div classname = "pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                     <div classname="graybox">
+                        <img id="blog" src="images/fake-profile-4.jpg"></img>
+                        <h2>A summer Friday hotspot</h2>
+                        <p>I had so much fun hosting LOFT’s first 97 Days of Summer in-store event
+                        this past Friday in NYC. I was all ruffles and smiles in a one shoulder blouse
+                        and wrap skirt.</p>
+                        <a href="full-post.js">read more</a>
+                     </div>
+                  </div>
            </div>
            </div>
     </div>
